dd2cf593aba576ed300acc1bb80757e6e9c32acc
Jose Miguel Pulgarin A <jjosemii64@gmail.com>
2024-10-13 17:12:54 -0500

    Changing photo layout and adding logic to transform images

---
+++ src/helpers/applyTransformationToImage.js
@@ -0,0 +1,20 @@
+import { getCldImageUrl } from "astro-cloudinary/helpers"
+
+export function applyTransformationToImage({id, imgToTransform, costumeTopic, backgroundTopic}) {
+  if (costumeTopic === null) return
+
+  const transformationsSettings = {
+    src: id,
+    replace: ['Clothes', `${costumeTopic} costume`]
+  }
+
+  if (backgroundTopic) transformationsSettings.replaceBackground = backgroundTopic
+
+  console.log(transformationsSettings)
+  const url = getCldImageUrl(transformationsSettings)
+  imgToTransform.style.opacity = ".3"
+  imgToTransform.src = url
+  imgToTransform.onload = () => {
+    imgToTransform.style.opacity = "1"
+  }
+}
